70c4853c56d494c306d5f7d67f113aa9938d68e6
GraceRuan <grace.ruan@gov.bc.ca>
2024-07-30 15:40:07 -0700

    fix: json output

--- .github/workflows/build-intention.sh
+++ .github/workflows/build-intention.sh
@@ -6,5 +6,5 @@ cat ./.github/workflows/build-intention.json | jq "\
     .event.reason=\"${EVENT_REASON}\" | \
     .event.url=\"https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}\" | \
     (.actions[] | select(.id == \"login\") .service.project) |= \"${PROJECT_NAME}\" | \
-    (.actions[] | select(.id == \"login\") .service.name) |= \"${SERVICE_NAME}\" | \
+    (.actions[] | select(.id == \"login\") .service.name) |= \"${SERVICE_NAME}\" \
     " > intention.json
